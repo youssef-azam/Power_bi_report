eec3b25b52e6b1be2af5588d5bbe6b71380e1c09
youssef azam <108946956+youssef-azam@users.noreply.github.com>
2024-01-27 17:38:06 +0200

    Add files via upload

---
+++ po.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+# Set page configuration
+st.set_page_config(page_title="Your App", page_icon=":rocket:", layout="wide")
+
+# Define the Power BI dashboard URL
+power_bi_dashboard_url = "https://app.powerbi.com/reportEmbed?reportId=f4fd2081-2550-4ea1-81cf-ccba67fc9472&autoAuth=true&ctid=d1aad15a-5724-45cd-a320-5e75718fa6bd"
+
+# Use Streamlit to display the Power BI dashboard using Markdown and HTML
+st.markdown(f'<iframe width="1100" height="600" src="{power_bi_dashboard_url}"></iframe>', unsafe_allow_html=True)
+
+# Additional Streamlit app code can be added here
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    st.write("Hello, Streamlit APP")
+    st.write("Rport vilda from 1-1-2024 to 27-1-2024")
+    st.write("I hope project like you ")
